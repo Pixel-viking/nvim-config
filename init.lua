1e123967fdc8375e188476d39bb0d416f6d95896
Pixel-Viking <cajo034@student.kristiania.no>
2025-10-26 16:17:28 +0000

    Previous commit did not contain the updated init.lua file

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ require("config.global")
 require("config.keyremap.general")
 require("config.keyremap.lsp_keymaps")
 require("config.keyremap.set_current_proj")
+require("config.keyremap.supermaven")
 require("config.lazy")
 
 
